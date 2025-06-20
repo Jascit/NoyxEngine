3ae16e55eca27e98caf786b1e08317e0317eee2b
Jacit <112516283+Jascit@users.noreply.github.com>
2025-06-20 13:47:37 +0200

    Update is_signed.hpp

--- Noyx/include/core/xstl/type_traits/is_signed.hpp
+++ Noyx/include/core/xstl/type_traits/is_signed.hpp
@@ -1,28 +1,19 @@
 #pragma once
 
 #include <type_traits/integral_constant.hpp>
+#include <type_traits/remove_cv.hpp>
 
 namespace xstl {
+  namespace details {
+    template<typename T>
+    struct _is_signed_helper {
+      constexpr static bool value = (static_cast<T>(-1)) < (static_cast<T>(0));
+    };
+  } // details
 
-  template <typename T>
-  struct is_signed : xstl::false_type {};
+  template<typename T>
+  struct is_signed : bool_constant<details::_is_signed_helper<remove_cv_t<T>>::value> {};
 
-  template <>
-  struct is_signed<signed char> : xstl::true_type {};
-
-  template <>
-  struct is_signed<short> : xstl::true_type {};
-
-  template <>
-  struct is_signed<int> : xstl::true_type {};
-
-  template <>
-  struct is_signed<long> : xstl::true_type {};
-
-  template <>
-  struct is_signed<long long> : xstl::true_type {};
-
-  template <typename T>
-  constexpr bool is_signed_v = is_signed<T>::value;
-
-}
+  template<typename T>
+  constexpr bool is_signed_v = details::_is_signed_helper<remove_cv_t<T>>::value;
+} // xstl
