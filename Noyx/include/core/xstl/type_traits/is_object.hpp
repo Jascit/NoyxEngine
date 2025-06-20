2da04a92fd69bff1dbf60c616d07687e2c09ead0
Jacit <andrijm3@gmail.com>
2025-06-20 23:04:12 +0200

    tests and fixes

--- Noyx/include/core/xstl/type_traits/is_object.hpp
+++ Noyx/include/core/xstl/type_traits/is_object.hpp
@@ -5,7 +5,7 @@
 
 namespace xstl {
   template<typename T>
-  struct is_object : bool_constant<!is_void_v<T> || !is_reference_v<T> || !is_function_v<T>> {};
+  struct is_object : bool_constant<!is_void_v<T> && !is_reference_v<T> && !is_function_v<T>> {};
   template<typename T>
   constexpr bool is_object_v = is_object<T>::value;
 }
