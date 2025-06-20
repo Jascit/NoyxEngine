fd3552248f11bcc76341ba98d3ecb1c72be1f4ce
Jacit <112516283+Jascit@users.noreply.github.com>
2025-06-20 13:45:43 +0200

    Update negation_test.cpp

--- tests/core/xstl/type_traits_tests/source/negation_test.cpp
+++ tests/core/xstl/type_traits_tests/source/negation_test.cpp
@@ -2,17 +2,17 @@
 #include <type_traits/is_same.hpp>
 #include <tt_test_detail.hpp>
 
-struct TrueType { static constexpr bool value = true; };
-struct FalseType { static constexpr bool value = false; };
+struct NegationUnitTestTrueType { static constexpr bool value = true; };
+struct NegationUnitTestFalseType { static constexpr bool value = false; };
 
 template <typename B, bool Expected>
 constexpr void tt_negation_test_type() {
-  static_assert(xstl::negation<B>::value == Expected, "negation returned wrong value");
-  static_assert(xstl::negation_v<B> == Expected, "negation_v returned wrong value");
+  NOYX_ASSERT_TRUE_MESSAGE(xstl::negation<B>::value == Expected, "negation returned wrong value");
+  NOYX_ASSERT_TRUE_MESSAGE(xstl::negation_v<B> == Expected, "negation_v returned wrong value");
   NOYX_ASSERT_TRUE_MESSAGE(xstl::negation<B>::value == Expected, "negation returned wrong value");
 }
 
 NOYX_TEST(Negation, UnitTest) {
-  tt_negation_test_type<TrueType, false>();
-  tt_negation_test_type<FalseType, true>();
+  tt_negation_test_type<NegationUnitTestTrueType, false>();
+  tt_negation_test_type<NegationUnitTestFalseType, true>();
 }
