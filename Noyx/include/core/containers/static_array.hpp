398cdcc4f2c8f45ea5ceba7594836debf1a2d1a9
MaksymRbkh <maxymriabykh@gmail.com>
2025-11-13 23:04:48 +0200

    static

--- Noyx/include/core/containers/static_array.hpp
+++ Noyx/include/core/containers/static_array.hpp
@@ -8,229 +8,65 @@
 #include "iterators/iterators.hpp"
 
 namespace noyx {
-  namespace containers {
+	namespace containers {
+		template<typename T, size_t N>
+		class StaticArray {
+		public:
+			T data[N];
+			size_t size__ = N;
 
-    template<typename T>
-    class TStaticArray {
-      using allocator_type = std::allocator<T>;// TODO: add StaticAllocator
-      using alloc_traits = std::allocator_traits<allocator_type>;
+		public:
+			StaticArray() = default;
 
-    public:
-      using value_type = T;
-      using pointer = typename alloc_traits::pointer;
-      using const_pointer = typename alloc_traits::const_pointer;
-      using size_type = typename alloc_traits::size_type;
-      using difference_type = typename alloc_traits::difference_type;
-      using reference = T&;
-      using const_reference = const T&;
-      using iterator = _StaticArrayIterator<T>;
-      using const_iterator = _StaticArrayConstIterator<T>;
+			StaticArray(const StaticArray& other) {			//copy-constructor
+				for (size_t i = 0; i < N; i++) {
+					data[i] = other.data[i];
+				}
+			}
 
-      static_assert(std::is_object_v<T>,
-        "T must be an object type (not void, function, reference, or incomplete type)");
+			StaticArray(StaticArray&& other) {				//move-constructor
+				for (size_t i = 0; i < N; i++) {
+					data[i] = std::move(other.data[i]);
+				}
+			}
 
-    public:
-      // ---- ctor
-      explicit TStaticArray(size_type size) : size_(size) {
-        if (size_ == 0) {
-          pair_.second() = nullptr;
-          return;
-        }
+			T& operator[](size_t i) { return data[i]; }
+			const T& operator[](size_t i) const { return data[i]; }
 
-        // allocate may throw, that's fine
-        pair_.second() = pair_.first().allocate(size_);
+		public:
+			size_t size() { return size__; }
+			bool empty() const { return size__ == 0; }
 
-        // if trivially default constructible -> nothing to do
-        if constexpr (std::is_trivially_default_constructible_v<T>) {
-          return;
-        }
+			T& at(size_t i) {								//returns index element
+				if (i < N) {
+					return data[i];
+				}
+				else throw std::out_of_range("Invalid index!");
+			}
+			const T& at(size_t i) const {
+				if (i < N) {
+					return data[i];
+				}
+				else throw std::out_of_range("Invalid index!");
+			}
 
-        // otherwise construct elements with rollback on exception
-        allocator_type& al = pair_.first();
-        size_type constructed = 0;
-        try {
-          for (; constructed < size_; ++constructed) {
-            alloc_traits::construct(al, pair_.second() + constructed);
-          }
-        }
-        catch (...) {
-          // destroy constructed in reverse order
-          for (size_type j = constructed; j > 0; --j) {
-            alloc_traits::destroy(al, pair_.second() + (j - 1));
-          }
-          al.deallocate(pair_.second(), size_);
-          pair_.second() = nullptr;
-          size_ = 0;
-          throw;
-        }
-      }
+			T& front() {									//returns first element
+				if (!empty()) return data[0];
+				else throw std::out_of_range("Array is empty!");
+			}
+			const T& front() const {
+				if (!empty()) return data[0];
+				else throw std::out_of_range("Array is empty!");
+			}
 
-      // ---- copy ctor (exception-safe, optimized for trivially_copyable)
-      TStaticArray(const TStaticArray& other) : size_(other.size_) {
-        if (size_ == 0) {
-          pair_.second() = nullptr;
-          return;
-        }
-
-        pair_.second() = pair_.first().allocate(size_);
-        allocator_type& al = pair_.first();
-        size_type constructed = 0;
-        try {
-          if constexpr (std::is_trivially_copyable_v<T>) {
-            std::memcpy(pair_.second(), other.pair_.second(), static_cast<size_t>(size_) * sizeof(T));
-            constructed = size_;
-          }
-          else {
-            for (; constructed < size_; ++constructed) {
-              alloc_traits::construct(al, pair_.second() + constructed, other.pair_.second()[constructed]);
-            }
-          }
-        }
-        catch (...) {
-          for (size_type j = constructed; j > 0; --j) {
-            alloc_traits::destroy(al, pair_.second() + (j - 1));
-          }
-          al.deallocate(pair_.second(), size_);
-          pair_.second() = nullptr;
-          size_ = 0;
-          throw;
-        }
-      }
-
-      // ---- move ctor
-      TStaticArray(TStaticArray&& other) noexcept {
-        // steal resources
-        pair_.copy(other.pair_);
-        size_ = other.size_;
-        other.pair_.second() = nullptr;
-        other.size_ = 0;
-      }
-
-      // ---- dtor
-      ~TStaticArray() {
-        if (pair_.second() != nullptr) {
-          allocator_type& al = pair_.first();
-          if constexpr (!std::is_trivially_destructible_v<T>) {
-            for (size_type i = 0; i < size_; ++i) {
-              alloc_traits::destroy(al, pair_.second() + i);
-            }
-          }
-          al.deallocate(pair_.second(), size_);
-          pair_.second() = nullptr;
-          size_ = 0;
-        }
-      }
-
-      // ---- copy-assignment (copy-and-swap = strong exception guarantee)
-      TStaticArray& operator=(const TStaticArray& other) {
-        if (this == &other) return *this;
-        TStaticArray tmp(other); // may throw, but strong guarantee
-        swap(tmp);
-        return *this;
-      }
-
-      // ---- move-assignment
-      TStaticArray& operator=(TStaticArray&& other) noexcept {
-        if (this == &other) return *this;
-
-        // destroy and deallocate current storage
-        clear_and_deallocate();
-
-        // steal other's resources
-        pair_.swap(other.pair_);
-        size_ = other.size_;
-        other.pair_.second() = nullptr;
-        other.size_ = 0;
-        return *this;
-      }
-
-      // ---- accessors
-      [[nodiscard]] reference operator[](size_type index) {
-        return *(pair_.second() + index);
-      }
-      [[nodiscard]] const_reference operator[](size_type index) const {
-        return *(pair_.second() + index);
-      }
-      [[nodiscard]] reference at(size_type index) {
-        return *(pair_.second() + index);
-      }
-      [[nodiscard]] const_reference at(size_type index) const {
-        return *(pair_.second() + index);
-      }
-      [[nodiscard]] reference front() {
-        return *pair_.second();
-      }
-      [[nodiscard]] const_reference front() const {
-        return *pair_.second();
-      }
-      [[nodiscard]] reference back() {
-        return *(pair_.second() + size_ - 1);
-      }
-      [[nodiscard]] const_reference back() const {
-        return *(pair_.second() + size_ - 1);
-      }
-      [[nodiscard]] pointer data() {
-        return pair_.second();
-      };
-      [[nodiscard]] const_pointer data() const {
-        return pair_.second();
-      };
-
-      // ---- iterators
-      [[nodiscard]] iterator begin() noexcept { return iterator(pair_.second(), 0, size_); }
-      [[nodiscard]] const_iterator begin() const noexcept { return const_iterator(pair_.second(), 0, size_); }
-      [[nodiscard]] iterator end() noexcept { return iterator(pair_.second() + size_, size_, size_); }
-      [[nodiscard]] const_iterator end() const noexcept { return const_iterator(pair_.second() + size_, size_, size_); }
-      [[nodiscard]] const_iterator cbegin() const noexcept { return const_iterator(pair_.second(), 0, size_); }
-      [[nodiscard]] const_iterator cend() const noexcept { return const_iterator(pair_.second() + size_, size_, size_); }
-
-      constexpr size_type size() const noexcept { return size_; }
-
-      // ---- fill: only for copy-assignable types
-      template<typename U = T>
-      std::enable_if_t<std::is_copy_assignable_v<U>, void>
-        fill(const T& value) {
-        for (size_type i = 0; i < size_; ++i) pair_.second()[i] = value;
-      }
-
-      // ---- swap
-      void swap(TStaticArray<T>& other) noexcept(
-        noexcept(std::declval<noyx::utility::TCompressedPair<allocator_type, T*>&>().swap(std::declval<noyx::utility::TCompressedPair<allocator_type, T*>&>()))
-        )
-      {
-        pair_.swap(other.pair_);
-        size_type tmp2_ = other.size_;
-        other.size_ = size_;
-        size_ = tmp2_;
-      }
-
-    private:
-      // helper to destroy elements and deallocate storage
-      void clear_and_deallocate() noexcept {
-        if (pair_.second() == nullptr) return;
-        allocator_type& al = pair_.first();
-        if constexpr (!std::is_trivially_destructible_v<T>) {
-          for (size_type i = 0; i < size_; ++i)
-            alloc_traits::destroy(al, pair_.second() + i);
-        }
-        al.deallocate(pair_.second(), size_);
-        pair_.second() = nullptr;
-        size_ = 0;
-      }
-
-    private:
-      noyx::utility::TCompressedPair<allocator_type, T*> pair_;
-      size_type size_ = 0;
-    };
-  } // namespace containers
-
-  namespace utility {
-    template<typename T>
-    constexpr void swap(containers::TStaticArray<T>& first, containers::TStaticArray<T>& second)
-      noexcept(noexcept(std::declval<containers::TStaticArray<T>&>().swap(std::declval<containers::TStaticArray<T>&>()))) {
-      first.swap(second);
-    }
-  }
-} // namespace noyx
-
-using noyx::containers::TStaticArray;
+			T& back() {										//returns last element
+				if (!empty()) return data[N - 1];
+				else throw std::out_of_range("Array is empty!");
+			}
+			const T& back() const {
+				if (!empty()) return data[N - 1];
+				else throw std::out_of_range("Array is empty!");
+			}
+		};
+	}
+}
