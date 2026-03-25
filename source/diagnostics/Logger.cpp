bd0bb118e9cba4446475a0fee26eedae06a841e5
Jacit <andrijm3@gmail.com>
2026-03-25 19:08:59 +0100

    Logger MVP

---
+++ source/diagnostics/Logger.cpp
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/*
+ * \file   Logger.cpp
+ * \brief  
+ *
+ * Copyright (c) 2026 Project Contributors
+ * \author Jascit <https://github.com/Jascit>
+ * \date   25.03.2026
+ * \note   
+ */
+
+#include <diagnostics/Logger.hpp>
+#include <fstream>
+#include <string>
+
+using namespace noyxcore::diagnostics;
+
+Logger::Logger(const char* log_file_name) : log_buffer_(64*1024) {
+  std::filesystem::path process_path = std::move(std::filesystem::current_path());
+  std::filesystem::path logs_path = process_path.parent_path().parent_path();
+  logs_path /= "logs";
+  if (!std::filesystem::exists(logs_path)) {
+    std::filesystem::create_directories(logs_path);
+  }
+  std::filesystem::path log_file_path = logs_path / log_file_name;
+  log_file_path_ = std::move(log_file_path);
+}
+
+bool Logger::log(const char* msg, LogLevel level) {
+  return log_buffer_.append(msg, level);
+}
+
+void Logger::flush() const {
+  std::ofstream log_file(log_file_path_);
+  if (log_file.is_open()) {
+    log_file << log_buffer_.cstr();
+  }
+}
